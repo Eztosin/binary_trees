f4bdbbd3933e5677113a11b073dd368ec4912144
root <tosinezekiel03@gmail.com>
2023-11-04 21:11:48 +0000

    Is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,15 +8,21 @@
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-if (tree == NULL)
-return (0);
+size_t left_height, right_height;
 
-if ((binary_tree_is_full(tree) == 1) && (binary_tree_balance(tree)
-					 == 0))
+if (binary_tree_is_full(tree))
+{
+left_height = binary_tree_height(tree->left);
+right_height = binary_tree_height(tree->right);
+
+if (left_height == right_height)
+{
 return (1);
-
+}
+}
 return (0);
 }
+
 
 /**
 * binary_tree_is_full - checks if a binary tree is full.
@@ -39,27 +45,6 @@ if (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right))
 return (1);
 }
 return (0);
-}
-
-/**
-* binary_tree_balance - measures the balance factor of a binary tree
-* @tree: pointer to the root node of the tree to measure the
-* balance factor.
-* Return: 0 If tree is NULL
-*/
-
-int binary_tree_balance(const binary_tree_t *tree)
-{
-size_t left_height = 0, right_height = 0;
-
-if (tree == NULL)
-{
-return (0);
-}
-
-left_height = binary_tree_height(tree->left);
-right_height = binary_tree_height(tree->right);
-return ((left_height - right_height));
 }
 
 /**
