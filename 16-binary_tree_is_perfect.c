727128289b34b6ff001dfb2d4010ceed40da00b7
root <tosinezekiel03@gmail.com>
2023-11-05 17:00:19 +0000

    Update 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -23,12 +23,10 @@ return (1);
 return (0);
 }
 
-
 /**
 * binary_tree_is_full - checks if a binary tree is full.
 * @tree: pointer to the root node of the tree to check.
 * Return: 0 If tree is NULL otherwise return 1
-*
 */
 
 int binary_tree_is_full(const binary_tree_t *tree)
@@ -38,12 +36,16 @@ if (tree == NULL)
 return (0);
 }
 
-if ((tree->left == NULL) && (tree->right == NULL))
-return (1);
-if (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right))
+if (tree->left == NULL && tree->right == NULL)
 {
 return (1);
 }
+
+if (tree->left && tree->right)
+{
+return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+}
+
 return (0);
 }
 
